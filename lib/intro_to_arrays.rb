f3b9c2944e0deb5c70d897f33b8a7c45908be6e2
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-16 09:05:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,9 +7,16 @@ def array_with_two_elements
   [1, 2]
 end
 
-def indexing(array)
+def first_element(array)
   array[0]
 end
 
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
 
 
